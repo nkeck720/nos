2050765458bfef5711bf25b2e26558ee7ca68ff4
Noah Keck <noahkeck72@gmail.com>
2015-09-20 14:52:16 -0400

    compile.sh update

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@
 # Compiles NOS to /dev/fd0
 #
 
-fasm myos.asm myos.img || exit
-fasm fsb.asm fsb.img || exit
-fasm kernel.asm kernel.img || exit
+fasm myos.asm myos.bin || exit
+fasm fsb.asm fsb.bin || exit
+fasm kernel.asm kernel.bin || exit
 fasm image.asm NOS.img || exit
 dd if=NOS.img of=/dev/fd0
 exit
