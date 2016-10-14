cdebc3dbdc1e389cab789a33f0e4ad8a4f936e45
Noah Keck <noahkeck72@gmail.com>
2016-10-14 08:40:50 -0400

    Added indication of Kernel Panic
    Made the kernel panic routine light up all the lock key lights to indicate when a panic happens.
    
    Signed-off-by: Nkeck720 <noahkeck72@gmail.com>

--- int21.asm
+++ int21.asm
@@ -281,7 +281,12 @@ nos_version:
 	mov cx, "20"
 	iret
 kernel_panic:
-	; Halt
+	; Turn on all keylights and Halt
+        mov dx, 60h 
+        mov al, 0EDh 
+        out dx, al
+	mov ax, 00000111b  
+        out dx, al
 	cli
 	hlt
 	jmp kernel_panic
