c40c79fa042d04d9f5926b316c15cbb24a17b7e4
nkeck720 <noahkeck72@gmail.com>
2016-11-27 13:33:26 -0500

    Fix some text formatting issues
    Signed-off-by: nkeck720 <noahkeck72@gmail.com>

--- text.asm
+++ text.asm
@@ -1,7 +1,13 @@
 	; This is an example of data file formatting. There is no beginning of file signature, however
 	; the end of file signature is signified by a 0xFF.
-	db "This is a text file stored under the NOS file system."
-	db "This file is for use with testing the 'type' command. It is also very"
-	db "convenient  for testing the file system's ability to track stored files."
+	db "This is a text file stored under the NOS file system.", 0Dh, 0Ah
+	db "This file is for use with testing the 'type' command. It is also very "
+	db "convenient for testing the file system's ability to track stored files."
+	; There should always be a newline at the end of text files
+	db 0Dh, 0Ah
 
 	db 0FFh
+	;--------------------------------
+	;Keep the thing in one sector
+	times 512-($-$$) db 0
+	
